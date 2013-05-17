78cae1b1d50a06ea6ec2d6b5d5b678af2e539ee4
Brian Clozel <brian.clozel@gmail.com>
2013-05-17 23:37:42 +0200

    reverse inits (moment should be init before)

--- js/main.js
+++ js/main.js
@@ -1,10 +1,6 @@
 require(['ui','teatime'], function(ui,teatime){
 
-    ui.init();
     teatime.init();
-
-    window.testdate = moment("2013-04-19 15:30");
-
-    window.teatime = teatime;
+    ui.init();
 
 });
